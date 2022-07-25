29a8cd9fcbcc50f7ed0304c66ca5737cba97e486
Michael Brewer <Michael.Brewer@provisionsgroup.com>
2022-07-25 15:09:47 -0600

    Added RPG awesome icons.

--- BBEG.Ui/src/app/about/about.component.ts
+++ BBEG.Ui/src/app/about/about.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from '@env/environment';
+import { RpgAwesomeIconsRegistry } from '@triangular/rpg-awesome-icons';
+import { rpgAwesomeIconAnvil, rpgAwesomeIconBleedingEye, rpgAwesomeIconCrystals } from '@triangular/rpg-awesome-icons/icons';
 import { MetadataService } from '../shell/metadata.service';
 
 @Component({
@@ -10,7 +12,14 @@ import { MetadataService } from '../shell/metadata.service';
 export class AboutComponent implements OnInit {
     version: string | null = environment.version;
 
-    constructor(private metadataService: MetadataService) { }
+    constructor(private metadataService: MetadataService,
+        private registry: RpgAwesomeIconsRegistry) {
+        registry.registerIcons([
+            rpgAwesomeIconAnvil,
+            rpgAwesomeIconBleedingEye,
+            rpgAwesomeIconCrystals
+        ]);
+    }
 
     ngOnInit() {
         this.metadataService.updateMetadata({
--- BBEG.Ui/src/app/about/about.module.ts
+++ BBEG.Ui/src/app/about/about.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { TranslateModule } from '@ngx-translate/core';
+import { RpgAwesomeIconsModule } from '@triangular/rpg-awesome-icons';
 import { MaterialModule } from '@app/material.module';
 import { SharedModule } from '@shared';
 import { AboutRoutingModule } from './about-routing.module';
@@ -11,6 +12,7 @@ import { AboutComponent } from './about.component';
     imports: [
         CommonModule,
         TranslateModule,
+        RpgAwesomeIconsModule,
         FlexLayoutModule,
         SharedModule,
         MaterialModule,
